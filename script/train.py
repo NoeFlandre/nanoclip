a5e9f1666d640e6b72724318a1a337513281c0bd
NoeFlandre <noeflandre@gmail.com>
2025-12-11 12:02:42 +0530

    print statement

--- script/train.py
+++ script/train.py
@@ -6,6 +6,7 @@ from torch.utils.data import DataLoader
 from config import Config 
 from torchvision import transforms
 from debug_dataset import CIFAR10CLIPDataset
+import time
 
 def main():
     cfg = Config()
@@ -35,6 +36,7 @@ def main():
     )
 
     dataloader = DataLoader(dataset=dataset, batch_size=cfg.batch_size, shuffle=True)
+    print(f"Training samples {len(dataloader.dataset)}")
 
     optimizer = torch.optim.AdamW(params=model.parameters(), lr=0.001)
 
@@ -44,15 +46,24 @@ def main():
         model.train()
         max_batch = 5
         for batch, (X, Y) in enumerate(dataloader):
+            t0 = time.time()
             if batch < max_batch :
-                print(f"Batch : {batch} / {len(dataloader)}")
                 X, Y = X.to(device), Y.to(device)
                 similarity, train_loss = model(image=X, text=Y)
-                train_loss_values.append(train_loss.item())
+                train_loss_value = train_loss.item()
+                train_loss_values.append(train_loss_value)
                 optimizer.zero_grad()
                 train_loss.backward()
+                norm = 0
+                for param in model.parameters():
+                    norm+=(torch.norm(param.grad))**2
+                norm = torch.sqrt(norm)
                 optimizer.step()
-        print(f"Epoch : {epoch} | Training Loss : {train_loss_values[-1]} ")
+                t1 = time.time()
+                dt = (t1-t0)*1000 #ms
+                print(f"Batch : {batch} / {len(dataloader)} | Time {dt:.0f} ms | Train Loss : {train_loss_value:.4f} | Grad Norm : {norm:.4f}")
+
+        print(f"Epoch : {epoch} | Training Loss : {train_loss_values[-1]:.4f} ")
 
 if __name__ == "__main__":
         main()
